3839ecd06b9083ca701f6d10f98b4fcbc736d556
Folafunmi <folafunmimustapha@gmail.com>
2022-12-11 04:31:47 +0100

    feat: add in fs-err

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let input = read_input().unwrap();
+    println!("{input}");
+}
+
+struct PathedIoError {
+    path: String,
+    inner: std::io::Error,
+}
+
+impl std::fmt::Debug for PathedIoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "for file {:?}: {}", self.path, self.inner)
+    }
+}
+
+fn read_input() -> Result<String, PathedIoError> {
+    let path = "src/input.txt";
+
+    match std::fs::read_to_string(path) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(PathedIoError {
+            path: path.into(),
+            inner: e,
+        }),
+    }
 }
